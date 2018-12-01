6f84ba8a4db36994dbba478f8a7d6b7a10d988ca
Michael James <mjames@pivotal.io>
2018-12-01 16:29:29 -0500

    Finished Options page.

--- Pcf.Demos.Steeltoe.Web/Controllers/Configuration/OptionsController.cs
+++ Pcf.Demos.Steeltoe.Web/Controllers/Configuration/OptionsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Pcf.Demos.Steeltoe.Web.Models;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using System;
+using System.Collections.Generic;
 
 namespace Pcf.Demos.Steeltoe.Web.Controllers.Configuration
 {
@@ -20,50 +22,55 @@ namespace Pcf.Demos.Steeltoe.Web.Controllers.Configuration
 
         public IActionResult Index()
         {
-            try
+            //read app settings
+            ViewBag.ApplicationId = appOptions.ApplicationId;
+            ViewBag.ApplicationName = appOptions.ApplicationName;
+            ViewBag.Uri = appOptions.ApplicationUris != null
+                && appOptions.ApplicationUris.Length > 0 ? appOptions.ApplicationUris[0] : "";
+            ViewBag.ApplicationVersion = appOptions.ApplicationVersion;
+            ViewBag.CloudFoundryApi = appOptions.CF_Api;
+            ViewBag.DiskLimit = appOptions.DiskLimit;
+            ViewBag.InstanceIndex = appOptions.InstanceIndex;
+            ViewBag.InstanceId = appOptions.InstanceId;
+            ViewBag.InstanceIp = appOptions.InstanceIP;
+            ViewBag.InternalIp = appOptions.InternalIP;
+            ViewBag.Limits = appOptions.Limits;
+            ViewBag.MemoryLimit = appOptions.MemoryLimit;
+            ViewBag.Name = appOptions.Name;
+            ViewBag.Port = appOptions.Port;
+            ViewBag.SpaceId = appOptions.SpaceId;
+            ViewBag.SpaceName = appOptions.SpaceName;
+            ViewBag.Start = appOptions.Start;
+            ViewBag.Uris = appOptions.Uris;
+            ViewBag.Version = appOptions.Version;
+
+            var services = new List<ServiceViewModel>();
+
+            //read service settings
+            if (serviceOptions.ServicesList != null && serviceOptions.ServicesList.Count > 0)
             {
-                //read app settings
-                ViewBag.ApplicationId = appOptions.ApplicationId;
-                ViewBag.ApplicationName = appOptions.ApplicationName;
-                ViewBag.Uri = appOptions.ApplicationUris != null
-                    && appOptions.ApplicationUris.Length > 0 ? appOptions.ApplicationUris[0] : "";
-                ViewBag.ApplicationVersion = appOptions.ApplicationVersion;
-                ViewBag.CloudFoundryApi = appOptions.CF_Api;
-                ViewBag.DiskLimit = appOptions.DiskLimit;
-                ViewBag.InstanceIndex = appOptions.InstanceIndex;
-                ViewBag.InstanceId = appOptions.InstanceId;
-                ViewBag.InstanceIp = appOptions.InstanceIP;
-                ViewBag.InternalIp = appOptions.InternalIP;
-                ViewBag.Limits = appOptions.Limits;
-                ViewBag.MemoryLimit = appOptions.MemoryLimit;
-                ViewBag.Name = appOptions.Name;
-                ViewBag.Port = appOptions.Port;
-                ViewBag.SpaceId = appOptions.SpaceId;
-                ViewBag.SpaceName = appOptions.SpaceName;
-                ViewBag.Start = appOptions.Start;
-                ViewBag.Uris = appOptions.Uris;
-                ViewBag.Version = appOptions.Version;
-
-                //read first service settings
-                if (serviceOptions.ServicesList != null && serviceOptions.ServicesList.Count > 0)
+                foreach (var service in serviceOptions.ServicesList)
                 {
-                    var serviceOption = serviceOptions.ServicesList[0];
-
-                    ViewData["ServiceLabel"] = serviceOption.Label;
-                    ViewData["ServiceName"] = serviceOption.Name;
-
-                    if (serviceOption.Credentials != null && serviceOption.Credentials.Count > 0)
+                    var serviceViewModel = new ServiceViewModel()
                     {
-                        ViewData["ClientId"] = serviceOption.Credentials["client_id"];
-                        ViewData["ClientSecret"] = serviceOption.Credentials["client_secret"];
-                        ViewData["ServiceUri"] = serviceOption.Credentials["uri"];
+                        Label = service.Label,
+                        Name = service.Name,
+                        Plan = service.Plan
+                    };
+
+                    foreach(var credential in service.Credentials)
+                    {
+                        if(credential.Key == "client_secret" || credential.Key == "password")
+                        {
+                            serviceViewModel.Credentials.Add("Client Secret", credential.Value.Value);
+                        }
                     }
+
+                    services.Add(serviceViewModel);
                 }
             }
-            catch (Exception ex)
-            {
-                ViewData["Message"] = ex.Message;
-            }
+
+            ViewBag.Services = services;
 
             return View();
         }
